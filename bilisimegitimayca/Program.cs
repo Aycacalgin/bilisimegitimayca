039a9368ea27fc3018762f64b3bb50eabee71b3a
bhr <bhr@DESKTOP-9IBCS2M>
2021-10-01 12:34:01 +0300

    Add project files.

---
+++ bilisimegitimayca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bilisimegitimayca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ yeniprojem/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace yeniprojem
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+
+        {
+            Console.WriteLine("Tebrikler.Urun Sepete eklendi:"+urun.Adi);
+        }
+        public void Ekle2(string urunAdi, string aciklama, double fiyat,int StokAdedi)
+        {
+            
+        }
+    }
+}
